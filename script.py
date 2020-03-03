ac6ba6a17462f9d6589bef6cf05ff59cf043b89c
BIZIOU Romain <biziou.romain@univ-catholyon.fr>
2020-03-03 14:31:22 +0100

    ajoutdallervasy

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ print('Hello Gauthier')
 print('Je me situe à coté du charmant Pierre Pellegrin !')
 print('Il est beau')
 print('Je me dois de rétablir la vérité :')
+print('allez vas-y')
 
 
 print('comment tu vas ?')
